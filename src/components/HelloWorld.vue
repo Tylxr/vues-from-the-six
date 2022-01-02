ba65d374e6592999628df2cdcaaf6cb9370e8eab
Tyler Marshall <tylerjeremiahh@gmail.com>
2022-01-02 17:20:27 +0000

    Building out the frontend of the Chat screen

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,75 @@
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
-    msg: String
-})
-
-const count = ref(0)
 </script>
 
 <template>
-    <p>Hello, this is the new text!</p>
-    <h1>{{ msg }}</h1>
+    <div>
+        <!-- <font-awesome-icon :icon="['fas', 'sms']" /> -->
+    </div>
 
-    <p>
-        Recommended IDE setup:
-        <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-        +
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    </p>
+    <!-- Navigation Header Component -->
+    <div class="nav-container flex justify-between bg-slate-100">
+        <div class="logo-container flex items-center p-7">
+            <ul class="flex justify-between text-3xl">
+                <li>
+                    <font-awesome-icon class="text-cyan-500" :icon="['fas', 'sms']" />
+                </li>
+                <li>
+                    <span class="italic">Chattr</span>
+                </li>
+            </ul>
+        </div>
+        <div class="navigation flex p-7 items-center">
+            <ul class="flex justify-between text-sm font-bold uppercase items-center">
+                <li class="ml-4 active">Chat</li>
+                <li class="ml-4">Conversations</li>
+                <li class="ml-4">Discover</li>
+                <li class="ml-4">
+                    <font-awesome-icon class="text-2xl" :icon="['far', 'bell']" />
+                </li>
+            </ul>
+        </div>
+    </div>
 
-    <p>
-        <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
-        |
-        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-    </p>
+    <!-- Chat Page/Route -->
+    <div class="chat-page flex h-full text-sm">
 
-    <button type="button" @click="count++">count is: {{ count }}</button>
-    <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
+        <!-- MiniConversation Component -->
+        <div class="mini-conversation-container w-1/3 bg-slate-400">
+
+            <!-- Search Component -->
+            <div class="search relative m-4">
+                <input
+                    type="text"
+                    class="bg-blue rounded-xl w-full h-full px-4 py-2 pl-7 focus:outline-none"
+                    placeholder="Search"
+                />
+                <font-awesome-icon
+                    class="text-slate-500 absolute top-0 left-2 h-full"
+                    :icon="['fas', 'sms']"
+                />
+            </div>
+
+            
+        </div>
+
+        <!-- Chat Component -->
+        <div class="chat-container w-2/3 bg-orange-300"></div>
+    </div>
 </template>
 
 <style scoped>
-a {
-    color: #42b983;
+.nav-container {
+    li {
+        margin-left: 12px;
+    }
+    .navigation {
+        li {
+            margin-left: 24px;
+
+            &.active {
+                color: rgb(6, 182, 212);
+            }
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSms } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount('#app')
+const icons = [faSms, faBell];
+library.add(icons);
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app');
